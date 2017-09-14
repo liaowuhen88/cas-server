735039ee9663c390c96ad0e33b8cd29bbbff22af
liaowuhen <296558063@qq.com>
2017-09-14 13:50:04 +0800

    增加监控

--- cas-server-webapp/src/main/java/org/jasig/cas/control/MonitorController.java
+++ cas-server-webapp/src/main/java/org/jasig/cas/control/MonitorController.java
@@ -1,11 +1,14 @@
 
 package org.jasig.cas.control;
 
+import org.jasig.cas.util.JSONUtil;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -14,8 +17,11 @@ public class MonitorController extends AbstractController {
 
     protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView mv = new ModelAndView();
-        mv.addObject("ALL_PASS=OK");
-        mv.setViewName("");
+        Map data = new HashMap();
+        data.put("success",true);
+        data.put("data","ALL_PASS=OK");
+        mv.addObject("data", JSONUtil.toJson(data));
+        mv.setViewName("doubao/ui/monitor");
 
 
         return mv;
